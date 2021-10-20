4fbfd87956fe8777619d8aebee9c9f480a3a6aeb
alexandre.demeure@univ-grenoble-alpes.fr <alexandre.demeure@univ-grenoble-alpes.fr>
2021-10-20 15:50:04 +0200

    init

---
+++ index.js
@@ -0,0 +1,31 @@
+console.log(`Hello Node.js v${process.versions.node}!`);
+
+const https = require('https');
+const fs = require("fs");
+const cors = require('cors')
+const express = require('express');
+const app = express()
+
+app.use(express.urlencoded({ extended: true }));
+app.use('/noCORS', express.static('static'))
+app.use('/withCORS', cors(), express.static('static'))
+
+
+app.listen(8080, () => console.log("A : Le serveur HTTP écoute sur le port 8080") );
+
+let priv, cert;
+if (!fs.existsSync("./private.key")) {
+    console.error("Pour démarrer le serveur HTTPS : Vous devriez avoir un fichier private.key contenant votre clef privé");    
+} else {
+    priv = fs.readFileSync("./private.key");
+}
+
+if (!fs.existsSync("./signature.cert")) {
+    console.error("Pour démarrer le serveur HTTPS : Vous devriez avoir un fichier signature.cert contenant votre clef privé");    
+} else {
+    cert = fs.readFileSync("./signature.cert");
+}
+
+if (!!priv && !!cert) {
+    https.createServer({key: priv, cert}, app).listen(8443, () => console.log("A : Le serveur HTTPS écoute sur le port 8443") )
+}
